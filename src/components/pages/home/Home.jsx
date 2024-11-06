624d96270dc1179773c13a1b073f9106a08ec04d
Jordao <jordandhunt01@gmail.com>
2024-11-06 10:23:57 +0000

    home layout

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faCss3Alt, faSquareJs,faNodeJs, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faCss3Alt, faSquareJs,faNodeJs, faPython, faGithub, faReact, faAngular, faVuejs, faPhp} from '@fortawesome/free-brands-svg-icons';
+import { faServer } from '@fortawesome/free-solid-svg-icons';
 import { faDatabase } from '@fortawesome/free-solid-svg-icons'
 import profilePicture from '../../../images/IMG_9712.jpg';
 import './Home.css';
 
 const Home = () => {
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
   return (
     <div className='home-page'>
       <div className="background-container">
@@ -24,11 +33,12 @@ const Home = () => {
           <div className="col">
           <h3>Hi! My name is Jordan and I'm a web developer from Essex, UK.</h3>
           <div className="button-container">
-          <Link to='/about'><button className='explore-btn'>About me</button></Link>
-          <Link to='/projects'><button className='explore-btn'>Explore Portfolio</button></Link>
+          <Link to='/about'><button onClick={scrollToTop} className='explore-btn'>About me</button></Link>
+          <Link to='/projects'><button onClick={scrollToTop} className='explore-btn'>Explore Portfolio</button></Link>
           </div>
           </div>
         </div>
+        <div className="skills">
         <div className="main frosted-container">
           <h2 className='title'>Here are my core languages:</h2>
           <div className="core-languages">
@@ -75,10 +85,59 @@ const Home = () => {
             </div>
           </div>
         </div>
+        </div>
+        <div className='skills'>
+        <div className="main frosted-container">
+          <h2 className='title'>Other technologies that I can use include:</h2>
+          <div className="other-languages">
+            <div className="github language-details">
+              <div className="icon icon-wiggle">
+                <FontAwesomeIcon icon={faGithub} />
+              </div>
+              <p>Github</p>
+            </div>
+            <div className="react language-details">
+              <div className="icon icon-wiggle2">
+                <FontAwesomeIcon icon={faReact} />
+              </div>
+              <p>React</p>
+            </div>
+            <div className="api language-details">
+              <div className="icon icon-wiggle2">
+                <FontAwesomeIcon icon={faServer} />
+              </div>
+              <p>APIs</p>
+            </div>
+          </div>
+        </div>
+        <div className="other frosted-container">
+          <h2 className='title'>I am now looking to study:</h2>
+          <div className="other-languages">
+            <div className="angular language-details">
+              <div className="icon icon-wiggle">
+                <FontAwesomeIcon icon={faAngular} />
+              </div>
+              <p>Angular</p>
+            </div>
+            <div className="vue language-details">
+              <div className="icon icon-wiggle2">
+                <FontAwesomeIcon icon={faVuejs} />
+              </div>
+              <p>Vue.js</p>
+            </div>
+            <div className="php language-details">
+              <div className="icon icon-wiggle2">
+                <FontAwesomeIcon icon={faPhp} />
+              </div>
+              <p>PHP</p>
+            </div>
+          </div>
+        </div>
+        </div>
       
       <div className="link-to-projects frosted-container">
         <h3>Click below to see my work!</h3>
-        <Link to='/projects'><button className='explore-btn'>Explore Portfolio</button></Link>
+        <Link to='/projects'><button onClick={scrollToTop} className='explore-btn'>Explore Portfolio</button></Link>
       </div>
     </div>
     </div>
