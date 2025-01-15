481f588fda54574a951205d492f111b9468137db
Jordao <jordandhunt01@gmail.com>
2025-01-15 12:30:52 +0000

    remove link import

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import './Sidebar.css';
