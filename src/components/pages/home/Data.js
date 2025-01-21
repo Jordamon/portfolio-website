29f0ffffc274d4eb505bccb168065512afb71e2c
Jordao <jordandhunt01@gmail.com>
2025-01-21 10:17:49 +0000

    homepage icon styles and tooltip

--- src/components/pages/home/Data.js
+++ src/components/pages/home/Data.js
@@ -7,28 +7,76 @@ import { faServer, faDatabase } from '@fortawesome/free-solid-svg-icons';
 
 // Core Languages
 export const coreLanguages = [
-  { name: 'HTML', icon: <FontAwesomeIcon icon={faHtml5} /> },
-  { name: 'CSS', icon: <FontAwesomeIcon icon={faCss3Alt} /> },
-  { name: 'JavaScript', icon: <FontAwesomeIcon icon={faSquareJs} /> },
+  { 
+    name: 'HTML', 
+    icon: <FontAwesomeIcon icon={faHtml5} />,
+    description: 'The structure of web pages' 
+  },
+  { 
+    name: 'CSS', 
+    icon: <FontAwesomeIcon icon={faCss3Alt} />,
+    description: 'Styling for websites' 
+  },
+  { 
+    name: 'JavaScript', 
+    icon: <FontAwesomeIcon icon={faSquareJs} />,
+    description: 'Adds interactivity to websites' 
+  },
 ];
 
 // Other Languages
 export const otherLanguages = [
-  { name: 'Node.js', icon: <FontAwesomeIcon icon={faNodeJs} /> },
-  { name: 'SQL', icon: <FontAwesomeIcon icon={faDatabase} /> },
-  { name: 'Python', icon: <FontAwesomeIcon icon={faPython} /> },
+  { 
+    name: 'Node.js', 
+    icon: <FontAwesomeIcon icon={faNodeJs} />,
+    description: 'JavaScript runtime for backend development' 
+  },
+  { 
+    name: 'SQL', 
+    icon: <FontAwesomeIcon icon={faDatabase} />,
+    description: 'Language for managing relational databases' 
+  },
+  { 
+    name: 'Python', 
+    icon: <FontAwesomeIcon icon={faPython} />,
+    description: 'General-purpose programming language' 
+  },
 ];
 
 // Other Technologies
 export const technologies = [
-  { name: 'GitHub', icon: <FontAwesomeIcon icon={faGithub} /> },
-  { name: 'React', icon: <FontAwesomeIcon icon={faReact} /> },
-  { name: 'APIs', icon: <FontAwesomeIcon icon={faServer} /> },
+  { 
+    name: 'GitHub', 
+    icon: <FontAwesomeIcon icon={faGithub} />,
+    description: 'Version control and collaboration platform' 
+  },
+  { 
+    name: 'React', 
+    icon: <FontAwesomeIcon icon={faReact} />,
+    description: 'JavaScript library for building user interfaces' 
+  },
+  { 
+    name: 'APIs', 
+    icon: <FontAwesomeIcon icon={faServer} />,
+    description: 'Protocols for communication between applications' 
+  },
 ];
 
 // Learning Technologies
 export const learningTechnologies = [
-  { name: 'Angular', icon: <FontAwesomeIcon icon={faAngular} /> },
-  { name: 'Vue.js', icon: <FontAwesomeIcon icon={faVuejs} /> },
-  { name: 'PHP', icon: <FontAwesomeIcon icon={faPhp} /> },
+  { 
+    name: 'Angular', 
+    icon: <FontAwesomeIcon icon={faAngular} />,
+    description: 'A web application framework powered by TypeScript' 
+  },
+  { 
+    name: 'Vue.js', 
+    icon: <FontAwesomeIcon icon={faVuejs} />,
+    description: 'Progressive JavaScript framework for building UIs' 
+  },
+  { 
+    name: 'PHP', 
+    icon: <FontAwesomeIcon icon={faPhp} />,
+    description: 'Server-side scripting language for web development' 
+  },
 ];
--- src/components/pages/home/LanguageSection.jsx
+++ src/components/pages/home/LanguageSection.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import { motion } from 'framer-motion'
+import { motion } from 'framer-motion';
 import './Home.css';
 
 const LanguageSection = ({ title, languages }) => (
-    <motion.div
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.6 }}
-      viewport={{ once: true }}
-    >
-  <div className="language-section frosted-container individual-item">
-    <h2 className="title">{title}</h2>
-    <div className="language-list">
-      {languages.map(({ name, icon }) => (
-        <div key={name} className="language-card">
-          <div className="icon">
-            {icon}
+  <motion.div
+    initial={{ opacity: 0, y: 50 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.6 }}
+    viewport={{ once: true }}
+  >
+    <div className="language-section frosted-container individual-item">
+      <h2 className="title">{title}</h2>
+      <div className="language-list">
+        {languages.map(({ name, icon, description }) => (
+          <div key={name} className="language-card" title={description}>
+            <div className="icon">
+              {icon}
+            </div>
+            <p>{name}</p>
+            <div className="tooltip">{description}</div>
           </div>
-          <p>{name}</p>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
   </motion.div>
 );
 
