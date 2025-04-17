2baa15caf4fb90273d22be27ee1da6dad087061b
Jordao <jordandhunt01@gmail.com>
2025-04-17 11:04:57 +0100

    update contact methods

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -13,9 +13,7 @@ const sidebar = () => {
           </a>        
           </div>
         <div className="icon icon-wiggle">
-        <a href="https://wa.me/07846579793" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faWhatsapp} />
-          </a>
+          <FontAwesomeIcon icon={faEnvelope} className='icon'/>
           </div>        
         <div className="icon icon-wiggle">
         <a href="https://www.instagram.com/jordamon_/" target="_blank" rel="noopener noreferrer">
