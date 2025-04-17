90180ecd8e9d3ed60feae82b91572147d3ebaed2
Jordao <jordandhunt01@gmail.com>
2025-04-17 11:08:33 +0100

    update contact methods

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import './Sidebar.css';
 
 const sidebar = () => {
